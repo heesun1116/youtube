5004c9331f257d9278052b0e252ef36147e265eb
Heesun <heeson0924@gmail.com>
2021-01-24 15:36:06 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
+
 
 function App() {
-  return <h1>HEllo :)</h1>
-}
+  const [videos, setVideos] = useState([]);
+   
+  
 
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAuRiyS9bfrflKaxpjk8pJ4jHTdW6wZUHQ", 
+      requestOptions
+      )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+     
+  },[]); //[] mount에만 호출, [a] a가 업데이트 될때마다 호출
+  
+  
+  //component,prop가 mount,update시에만 호출
+
+  return <VideoList videos = {videos}/>
+}
 export default App;
